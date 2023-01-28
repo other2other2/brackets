b48b456906d2f6b77d98628494bc0aa1d5b75461
Vladislav Lekontsev <other2other2@gmail.com>
2023-01-28 04:51:54 +0400

    feat: complete brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = [];
+  const opens = bracketsConfig.map(pairBracket => pairBracket[0]);
+  const ends = bracketsConfig.map(pairBracket => pairBracket[1]);
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (ends.includes(str[i]) && (ends.indexOf(str[i]) == opens.indexOf(brackets.at(-1)))) {
+      brackets.pop();
+    } else if (opens.includes(str[i])) {
+      brackets.push(str[i]);
+    } else return false
+  }
+
+  return (brackets.length == 0) ? true : false;
 }
